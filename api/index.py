5fc359f474e16a4aeda4966df2ae7826e564cbe3
Ema <faunapescaycaza@gmail.com>
2025-11-24 21:57:23 -0300

    Debug: Re-add logging for API request path

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
@@ -83,6 +83,7 @@ def get_drive_files():
 
 @app.route('/drive-files')
 def list_drive_files():
+    print(f"Request path received by Flask: {request.path}")
     """API endpoint to get the list of files from Google Drive."""
     try:
         files = get_drive_files()
